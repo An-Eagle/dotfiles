75924ab03cbf2a143416994329d610eef12c1083
An-Eagle <nicolas@cowley.tech>
2025-10-25 10:13:20 +0200

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -2,4 +2,7 @@ require("nicolas")
 vim.lsp.enable({
     'lua_ls',
 })
+vim.keymap.set('n', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.keymap.set('v', '<leader>y', '"+y', { noremap = true, silent = true })
+
 
